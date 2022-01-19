9ee48691622c232a701ac8be12299fcd7d372052
d <d@senseworldcafe.com>
2022-01-19 11:22:56 -0700

    uploading all django files and python

---
+++ bigtest/bigtest/ml_predict.py
@@ -0,0 +1,6 @@
+def prediction_model(pclass,sex,age,sibsp,parch,fare,embarked,title):
+    import pickle
+    x = [[pclass,sex,age,sibsp,parch,fare,embarked,title]]
+    randomforest = pickle.load(open('titanic_model.sav', 'rb'))
+    predictions = randomforest.predict(x)
+    return predictions
